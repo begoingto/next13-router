a40585358aaf4c492ffbdbd9b10cbc922a71c284
Begoingto <begoingto.me@gmail.com>
2023-05-10 13:41:55 +0700

    add flowbit react🆗✅

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,12 @@
+'use client';
 import Link from "next/link";
+import { Alert } from "flowbite-react";
 
 export default function Dashboard(){
     return (
         <main className="dashboard">
             <h1>Welcome to Dashboard</h1>
+            <Alert color="info">Alert!</Alert>;
         </main>
     )
 }
